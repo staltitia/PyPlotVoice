26c993470a1a06110bbbea0889cbf2576602a03b
staltitia <staltitia@yahoo.com>
2014-11-28 21:05:42 -0800

    split code into functions, added silence barchart

--- PyPlotVoice.py
+++ PyPlotVoice.py
@@ -13,8 +13,66 @@ import numpy
 import matplotlib.pyplot as plt
 
 WINDOW_WIDTH = 1 #in seconds
-SIL_THRESH = 0.5 #in seconds
+SIL_THRESH = 0.3 #in seconds
 NORM_CONV_THRESH = 0.1 #normalised
+
+def ConvSilence( inXArray, inYArray, samplingRate ):		
+
+	if inXArray.size != inYArray.size:
+		print "Error in ConvSilence: Dimensions must agree"
+		return [ None, None, None, None ]
+
+	#convWin = numpy.ones( WINDOW_WIDTH * samplingRate )
+	convWin = signal.triang( WINDOW_WIDTH * samplingRate )
+	
+	starProduct = numpy.convolve( abs(inYArray), convWin, 'same' )
+	starProduct = starProduct / max(starProduct)
+
+	silenceList = starProduct < NORM_CONV_THRESH
+
+	threshold = SIL_THRESH * samplingRate
+		
+	silence = True
+	silList = []
+	for i in range(silenceList.size):
+		risingEdge = silence and not silenceList[i]
+		fallingEdge = not silence and silenceList[i]
+		if (risingEdge or fallingEdge):
+			silence = not silence
+			if ( len(silList) > 0 and i - silList[-1] < threshold ):
+				silList.pop()
+			else:
+				silList.append(i)	
+
+	toRetX = numpy.split(inXArray,silList)
+	toRetY = numpy.split(inYArray,silList)
+
+	#now we split between evens and odds
+	#because we have silence=True to start,
+	#the evens are silent, and the odds are not
+
+	silentX = toRetX[::2]
+	silentY = toRetY[::2]
+
+	speakX = toRetX[1::2]
+	speakY = toRetY[1::2]
+
+	#print "reading "+str(i+1)+" has "+str(len(silList)/2)+" pauses"
+
+	return [ silentX, silentY, speakX, speakY ]
+
+def SilenceCounter( inXArray, inXSilentList, samplingRate ):
+	
+	arrListX = numpy.split( inXArray, numpy.arange(samplingRate*60, inXArray.size, samplingRate*60) )
+	count = []
+	silList = numpy.concatenate( inXSilentList )
+	for xList in arrListX:
+		count.append( numpy.intersect1d( xList, silList, assume_unique = True ).size )
+
+	return numpy.array(count) 
+
+def splitter( inX, inY, samplingRate, function ):
+	return function( inX, inY, samplingRate )
 
 def main():
 	filename = ''
@@ -39,81 +97,58 @@ def main():
 	xaxis = numpy.arange( 0, float(len(values)) / samplingRate, 1.0 / samplingRate )
 
 	arrListX = numpy.split(xaxis, [ 60*2*samplingRate, 60*10*samplingRate, 60*12*samplingRate ] )
-	
-	toPlotX = []
-	toPlotY = []
 
-	#convWin = numpy.ones( WINDOW_WIDTH * samplingRate )
-	convWin = signal.triang( WINDOW_WIDTH * samplingRate )
-	starProduct1 = numpy.convolve( abs(arrListY[1]), convWin, 'same' )
-	starProduct1 = starProduct1 / max(starProduct1)
-	starProduct2 = numpy.convolve( abs(arrListY[3]), convWin, 'same' )
-	starProduct2 = starProduct2 / max(starProduct2)
+	[ans1SilX, ans1SilY, ans1SpkX, ans1SpkY] = splitter( arrListX[1], arrListY[1], samplingRate, ConvSilence )
+	[ans2SilX, ans2SilY, ans2SpkX, ans2SpkY] = splitter( arrListX[3], arrListY[3], samplingRate, ConvSilence )
 
-	silenceList = []
-	silenceList.append(starProduct1 < NORM_CONV_THRESH)
-	silenceList.append(starProduct2 < NORM_CONV_THRESH)		
+	ans1Bucket = SilenceCounter( arrListX[1], ans1SilX, samplingRate )
+	ans2Bucket = SilenceCounter( arrListX[3], ans2SilX, samplingRate ) 
 
-	threshold = SIL_THRESH * samplingRate
-	for i in range (2):
-		silence = True
-		period = i*2+1
-		silList = []
-		truthList = silenceList[i]
-		for j in range(truthList.size):
-			risingEdge = silence and not truthList[j]
-			fallingEdge = not silence and truthList[j]
-			if (risingEdge or fallingEdge):
-				silence = not silence
-				if ( j - silList[-1] < threshold ):
-					silList.pop()
-				else:
-					silList.append(j)
-
-		toPlotX.append( numpy.split(arrListX[period],silList) )
-		toPlotY.append( numpy.split(arrListY[period],silList) )
-		print "reading "+str(i+1)+" has "+str(len(silList)/2)+" pauses"
-	
-	plt.clf()		
+	plt.clf()
 	plt.hold(True)
 	#for the reading parts
 	#colours: green for read, blue for speak, red for pause in answering
 	plt.plot( arrListX[0], arrListY[0], 'g', arrListX[2], arrListY[2], 'g')
-	for i in range(len(toPlotX)):
-		silence = True 
-		xList = toPlotX[i]
-		yList = toPlotY[i]
-		for j in range(len(xList)):
-			if (silence):
-				plt.plot( xList[j], yList[j], 'r')
-				silence = False
-			else:
-				plt.plot( xList[j], yList[j], 'b')
-				silence = True
+	
+	for i in range(len(ans1SilX)):
+		plt.plot( ans1SilX[i], ans1SilY[i], 'r' )
+	for i in range(len(ans1SpkX)):
+		plt.plot( ans1SpkX[i], ans1SpkY[i], 'b' )
+	for i in range(len(ans2SilX)):
+		plt.plot( ans2SilX[i], ans2SilY[i], 'r' )
+	for i in range(len(ans2SpkX)):
+		plt.plot( ans2SpkX[i], ans2SpkY[i], 'b' )
 	plt.savefig( "silences.png" )
 
-	#individual answer plots
-	for i in range(len(toPlotX)):
-		plt.clf()
-		silence = True 
-		xList = toPlotX[i]
-		yList = toPlotY[i]
-		for j in range(len(xList)):
-			if (silence):
-				plt.plot( xList[j], yList[j], 'r')
-				silence = False
-			else:
-				plt.plot( xList[j], yList[j], 'b')
-				silence = True
-		plt.savefig( "silences"+str(i+1)+".png" )
-	
-	#for debugging purposes
-	#plt.hold(False)
-	#plt.plot(arrListX[1], starProduct1)
-	#plt.savefig( "Convolution 1.png" )
-	#plt.plot(arrListX[3], starProduct2)
-	#plt.savefig( "Convolution 2.png" )
+	#minute by minute save
 
+	for i in range(20):
+		plt.xlim( [ i*60, (i+1)*60 ] )
+		plt.savefig( "silences_minute_"+str(i+1)+".png" )
+
+	#individual answer plots
+	plt.xlim( [ 2*60, 10*60 ] )
+	plt.savefig( "silences1.png" )
+	
+	plt.xlim( [ 12*60, 20*60 ] )
+	plt.savefig( "silences2.png" )
+
+	#for minute-based silence bar chart
+	plt.clf()
+	percentage =  ans1Bucket * 100 / ( samplingRate * 60 )
+	plt.bar( numpy.arange(0.6,ans1Bucket.size+0.6, 1), percentage, color='r' )
+	plt.ylim( [0, 120] )
+	plt.ylabel( "Percentage of time silent" )
+	plt.xlabel("Time (minutes)")
+	plt.savefig( "silBarChart1.png" )
+
+	plt.clf()
+	percentage =  ans2Bucket * 100 / ( samplingRate * 60 )
+	plt.bar( numpy.arange(0.6,ans2Bucket.size+0.6, 1), percentage, color='r' )
+	plt.ylim( [0, 120] )
+	plt.ylabel( "Percentage of time silent" )
+	plt.xlabel("Time (minutes)")
+	plt.savefig( "silBarChart2.png" )
 
 if __name__ == "__main__":
 	main()
